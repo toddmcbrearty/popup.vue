7da6a010c9666a817a117778e236490dac80e542
Todd <todd.mcbrearty@gmail.com>
2018-04-18 14:29:20 -0400

    moved components

---
+++ src/components/_components/Copyright.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="date">
+    <span v-html="copy"></span>
+    <span v-text="copyDate"></span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Copyright",
+
+    data() {
+      return {
+        copy: '&copy;'
+      }
+    },
+
+    computed: {
+      copyDate() {
+        return new Date().getFullYear();
+      }
+    },
+  }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/_components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="header hero is-primary">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title" v-text="name"></h1>
+        <h2 class="subtitle" v-show="showHeadline" v-text="headline"></h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+
+    props: {
+      name: {
+        type: String,
+        default: "Default",
+      },
+      headline: {
+        type: String,
+        default: "Default headline text",
+      },
+    },
+
+    data() {
+      return {
+        showHeadline: {
+          type: Boolean,
+          value: this.headline !== '',
+        },
+      };
+    },
+  };
+</script>
+
+<style lang="scss">
+  @import '../../scss/overrideables';
+
+  .header {
+    font-family: $secondaryFont;
+  }
+
+  .header .title, .header .subtitle {
+    color: #000 !important;
+  }
+
+</style>
